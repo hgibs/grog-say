0de4e25ee7281df3fbffac2e7f0d5f57c3874b76
Holland Gibson <holland@hollandgibson.com>
2024-07-18 23:04:53 -0400

    very simple rock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Clone, Debug)]
+struct Quote {
+    message: String,
+    attribution: String,
+}
+
+impl Quote {
+    pub fn formatted(&self) -> String {
+        format!("\"{}\" \n - {}", self.message, self.attribution)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let default_case = Quote {
+        message: "grug say prototype early in software making, especially if many big brains"
+            .to_string(),
+        attribution: "grugbrain.dev".to_string(),
+    };
+
+    println!("{}", default_case.formatted())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_derive() {
+        let data = r#"
+            {
+                "message": "something",
+                "attribution": "@hgibs"
+            }
+        "#;
+        let single: Quote = serde_json::from_str(data).expect("bad test data");
+        assert_eq!("something", single.message);
+
+        println!("\"{}\" - {}", single.message, single.attribution);
+    }
 }
