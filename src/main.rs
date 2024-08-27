91e9332352c4ef222141ba5d2a7a81a807214c4b
Holland Gibson <holland@hollandgibson.com>
2024-08-27 11:59:05 -0400

    Quotes with schema can load

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use serde::Deserialize;
+use std::fmt::Debug;
+use std::fs;
 
 #[derive(Deserialize, Clone, Debug)]
 struct Quote {
@@ -22,9 +24,21 @@ fn main() {
     println!("{}", default_case.formatted())
 }
 
+fn load_quotes_file(file_path: &str) -> Vec<Quote> {
+    let file_contents = fs::read_to_string(file_path)
+        .expect(format!("Problem opening the file: {}", file_path).as_str());
+    println!("{}", file_contents);
+
+    match serde_json::from_str(file_contents.as_str()) {
+        Ok(result) => result,
+        Err(error) => panic!("Problem deserializing the json: {error:?}"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use log::debug;
 
     #[test]
     fn test_simple_derive() {
@@ -42,7 +56,16 @@ mod tests {
 
     #[test]
     fn test_load_file() {
-        let quotes: Vec<Quote> = load_file("quotes.json");
-        assert!(quotes.len() > 0)
+        let quotes: Vec<Quote> = load_quotes_file("quotes.json");
+        debug!("{:?}", quotes);
+        assert!(!quotes.is_empty());
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Problem deserializing the json: Error(\"missing field `message`\", line: 5, column: 3)"
+    )]
+    fn test_bad_file() {
+        let _quotes: Vec<Quote> = load_quotes_file("wrongschema.json");
     }
 }
